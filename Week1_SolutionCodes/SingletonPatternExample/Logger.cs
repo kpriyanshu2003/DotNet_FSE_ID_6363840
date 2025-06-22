a1ae9fe5a653ed065ba9943af095554d25019458
Kumar Priyanshu <az.priyanshu.rockstar@gmail.com>
2025-06-22 23:58:35 +0530

    Added week 1 solutions

---
+++ Week1_SolutionCodes/SingletonPatternExample/Logger.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class Logger
+{
+    private static Logger instance;
+
+    private Logger()
+    {
+        Console.WriteLine("Initialized Logger instance");
+    }
+
+    public static Logger GetInstance()
+    {
+        if (instance == null)
+        {
+            instance = new Logger();
+        }
+        return instance;
+    }
+
+    public void Log(string message)
+    {
+        Console.WriteLine("Log Message: " + message);
+    }
+}
---
+++ Week1_SolutionCodes/SingletonPatternExample/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger logger1 = Logger.GetInstance();
+        Logger logger2 = Logger.GetInstance();
+
+        logger1.Log("First message");
+        logger2.Log("Second message");
+
+        Console.WriteLine("Are both instances same? " +
+            (logger1 == logger2 ? "Yes, singleton works" : "No, singleton failed"));
+    }
+}

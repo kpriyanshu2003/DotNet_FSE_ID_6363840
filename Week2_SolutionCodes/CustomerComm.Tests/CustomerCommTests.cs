a9caa598608ea92c7abbca372a4a5fb227317a9b
Kumar Priyanshu <az.priyanshu.rockstar@gmail.com>
2025-06-29 22:53:03 +0530

    added week 2 solutions

---
+++ Week2_SolutionCodes/CustomerComm.Tests/CustomerCommTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using Moq;
+using CustomerCommLib;
+
+namespace CustomerComm.Tests
+{
+    [TestFixture]
+    public class CustomerCommTests
+    {
+        private Mock<IMailSender> _mockMailSender;
+        private CustomerCommLib.CustomerComm _customerComm;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            _mockMailSender = new Mock<IMailSender>();
+            _customerComm = new CustomerCommLib.CustomerComm(_mockMailSender.Object);
+        }
+
+        [TestCase]
+        public void SendMailToCustomer_WhenCalled_ReturnsTrue()
+        {
+            _mockMailSender.Setup(m => m.SendMail(It.IsAny<string>(), It.IsAny<string>())).Returns(true);
+
+            bool result = _customerComm.SendMailToCustomer();
+
+            Assert.IsTrue(result);
+            _mockMailSender.Verify(m => m.SendMail(It.IsAny<string>(), It.IsAny<string>()), Times.Once());
+        }
+    }
+}

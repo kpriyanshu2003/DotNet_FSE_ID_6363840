4c6c6e97c01ac7962148bf0435de344a66dd7d5b
Kumar Priyanshu <az.priyanshu.rockstar@gmail.com>
2025-06-29 22:52:52 +0530

    Added Week 2 solutions

---
+++ Week2_SolutionCodes/CalcLibrary/CalcLibrary.Tests/CalculatorTests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using CalcLibrary;
+
+namespace CalcLibrary.Tests
+{
+    [TestFixture]
+    public class CalculatorTests
+    {
+        private SimpleCalculator _calculator;
+
+        [SetUp]
+        public void Setup()
+        {
+            _calculator = new SimpleCalculator();
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _calculator = null;
+        }
+
+        [Test]
+        [TestCase(2.1, 3.7, 5.8)]
+        [TestCase(-1.3, -1.0, -2.3)]
+        [TestCase(0, 0, 0)]
+        public void Addition_AddsTwoNumbers_ReturnsCorrectResult(double a, double b, double expected)
+        {
+            var result = _calculator.Addition(a, b);
+            Assert.That(result, Is.EqualTo(expected).Within(0.0001));
+        }
+    }
+}
